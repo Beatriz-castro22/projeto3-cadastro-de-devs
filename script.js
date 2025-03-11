63033c201f1af5a50e59d813541e518a3ddfbeab
Bia★ <anabiafernandes.222@gmail.com>
2025-03-11 13:13:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,7 @@ form.addEventListener("register-button", function (ev){
     ev.preventDefault()
     function register(ev){
     const sectionElement = ev.currentTarget.parentNode
-    const username = sectionElement.children.username.value
-    const password = sectionElement.children.password.value
-    const passwordConfirmation =sectionElement.children.passwordConfirmation.value
     
-    if(password === passwordConfirmation) {
-        console.log(`usuario ${username} registrado!`)
-    } else{
-        console.log('as senhas não conferem!')
-    }
 }
 
 const button = document.getElementById('register-button')
